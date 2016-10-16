c1b2846b7a22bda9de675e411205f11e4aefb1f4
roman.jadroncik <roman.jadroncik@gmail.com>
2016-10-17 00:01:21 +0100

    Build script now figures out the path to the source files.

--- build.ps1
+++ build.ps1
@@ -1,15 +1,21 @@
+function Get-ScriptDirectory
+{
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  Split-Path $Invocation.MyCommand.Path
+}
+
 $devenv = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
-$shadow = "D:\source\Shadow\Shadow Common Files\"
+$shadow = Get-ScriptDirectory;
 $config = "Debug"
 
-&$devenv $shadow"Source\Mathematics\Mathematics.vcxproj" /Build $config
-&$devenv $shadow"Source\Timer\Timer.vcxproj" /Build $config
+&$devenv $shadow"\Source\Mathematics\Mathematics.vcxproj" /Build $config
+&$devenv $shadow"\Source\Timer\Timer.vcxproj" /Build $config
 
-&$devenv $shadow"Source\ObjectExtensions\ObjectExtensions.vcxproj" /Build $config | Out-Null
+&$devenv $shadow"\Source\ObjectExtensions\ObjectExtensions.vcxproj" /Build $config | Out-Null
 
-&$devenv $shadow"Source\Debugging\Debugging.vcxproj" /Build $config
-&$devenv $shadow"Source\Imaging\Imaging.vcxproj" /Build $config
-&$devenv $shadow"Source\Graphics\Graphics.vcxproj" /Build $config
-&$devenv $shadow"Source\XML\XML.vcxproj" /Build $config
-&$devenv $shadow"Source\DatafileIO\DatafileIO.vcxproj" /Build $config
-&$devenv $shadow"Source\Scripting\Scripting.vcxproj" /Build $config
+&$devenv $shadow"\Source\Debugging\Debugging.vcxproj" /Build $config
+&$devenv $shadow"\Source\Imaging\Imaging.vcxproj" /Build $config
+&$devenv $shadow"\Source\Graphics\Graphics.vcxproj" /Build $config
+&$devenv $shadow"\Source\XML\XML.vcxproj" /Build $config
+&$devenv $shadow"\Source\DatafileIO\DatafileIO.vcxproj" /Build $config
+&$devenv $shadow"\Source\Scripting\Scripting.vcxproj" /Build $config
